80172650695a345305cfb1328cfb93a8cd8f4d5e
Ranjithchodavarapu <60022275+ranjithchodavarapu@users.noreply.github.com>
2022-02-01 11:21:32 -0500

    Add files via upload

---
+++ 01-intro/query.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect("project.db")
+cursor = connection.cursor()
+
+rows = cursor.execute("select name, kind from pet, species where species_id = species.id").fetchall()
+#rows = list(rows)
+print(rows)
+#for i in rows:
+    #name, kind = i
+    #print(f"my kind is {kind} and name is {name}")
+
+print("completed..")
