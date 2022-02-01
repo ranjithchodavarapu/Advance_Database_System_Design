2c6ddeab489aa3013a0660b2272c2a0cfecb9fa8
Ranjith Chodavarapu <ranjuchodavarapu@gmail.com>
2022-02-01 11:37:34 -0500

    push

--- 01-intro/query.py
+++ 01-intro/query.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("project.db")
+connection = sqlite3.connect("pets.db")
 cursor = connection.cursor()
 
 rows = cursor.execute("select name, kind from pet, species where species_id = species.id").fetchall()
